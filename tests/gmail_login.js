f1beab413c4c055359287fb65f88bfddd569904a
aayush2400 <149470803+aayush2400@users.noreply.github.com>
2023-11-09 15:27:14 +0530

    gmail login test added

---
+++ tests/gmail_login.js
@@ -0,0 +1,38 @@
+import { initContext } from "../src/auto_page.js";
+import { closeBrowser } from "../src/init_browser.js";
+
+const path = "https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin";
+const elements = {
+  textbox_username: [
+    [{ role: ["textbox", { name: "Email or phone" }] }],
+    [
+      {
+        css: "input[type='email']",
+      },
+    ],
+  ],
+  button_next: [[{ role: ["button", { name: "Next" }] }], [{ css: "#identifierNext" }]],
+  textbox_password: [
+    [{ role: ["textbox", { name: "Enter your password" }] }],
+    [
+      {
+        css: "input[type='password']",
+      },
+    ],
+  ],
+  button_login: [[{ role: ["button", { name: "Next" }] }], [{ css: "#passwordNext" }]],
+};
+const context = await initContext(path, true, false);
+const login = async function () {
+  let info = null;
+  await context.stable.fill(elements.textbox_username, "testblinq.io@gmail.com");
+  info = await context.stable.click(elements.button_next);
+  await context.stable.waitForPageLoad();
+  await context.stable.fill(elements.textbox_password, "password@24");
+  info = await context.stable.click(elements.button_login);
+  await context.stable.waitForPageLoad();
+};
+await login();
+
+await new Promise((resolve) => setTimeout(resolve, 1000));
+await closeBrowser();
