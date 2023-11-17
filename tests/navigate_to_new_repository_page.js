701a9545fd044d567e56faee3bd140defe20cc6b
aayush2400 <149470803+aayush2400@users.noreply.github.com>
2023-11-17 13:10:16 +0530

    Added two new tests files
    Added two new tests files - create new repository and navigate to new repository

---
+++ tests/navigate_to_new_repository_page.js
@@ -0,0 +1,45 @@
+import { initContext } from "../src/auto_page.js";
+import { closeBrowser } from "../src/init_browser.js";
+
+const path = "https://github.com/login";
+const elements = {
+  textbox_username: [
+    [{ role: ["textbox", { name: "Username or email address" }] }],
+    [
+      {
+        css: "input[name='login']",
+      },
+    ],
+  ],
+  textbox_password: [
+    [{ role: ["textbox", { name: "Password" }] }],
+    [
+      {
+        css: "input[name='password']",
+      },
+    ],
+  ],
+  button_signin: [
+    [{ role: ["button", { name: "Sign in" }] }],
+    [
+      {
+        css: "input[type='submit']",
+      },
+    ],
+  ],
+};
+const context = await initContext(path, true, false);
+const loginAndNavigate = async function () {
+  let info = null;
+  await context.stable.fill(elements.textbox_username, "aayush2400");
+  await context.stable.fill(elements.textbox_password, "Blinqio@24");
+  info = await context.stable.click(elements.button_signin);
+  await context.stable.waitForPageLoad();
+
+  await context.stable.goto("https://github.com/new");
+  await context.stable.waitForPageLoad();
+};
+await loginAndNavigate();
+
+await new Promise((resolve) => setTimeout(resolve, 1000));
+await closeBrowser();
