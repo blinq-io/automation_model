5acfc6eeb6fe86b37fd4aeee77d473e4cb870d62
guy arieli <guy.arieli@blinq.io>
2023-11-23 12:29:58 +0200

    fix the experation issue

--- src/environment.ts
+++ src/environment.ts
@@ -1,21 +1,20 @@
-type Cookie ={
- name: string
- value: string
-  domain?: string
-  path?: string
-  expires?: number
-  httpOnly?: boolean
-  secure?: boolean
-  sameSite?: string
-}
+type Cookie = {
+  name: string;
+  value: string;
+  domain?: string;
+  path?: string;
+  expires?: number | string;
+  httpOnly?: boolean;
+  secure?: boolean;
+  sameSite?: string;
+};
 
-type LocalStorage = Record<string, string>
+type LocalStorage = Record<string, string>;
 
 class Environment {
-  cookies:Cookie[] = []
-  origins:{origin:string, localStorage:LocalStorage}[] = []
-  constructor(public baseUrl?: string) {
-  }
+  cookies: Cookie[] = [];
+  origins: { origin: string; localStorage: LocalStorage }[] = [];
+  constructor(public baseUrl?: string) {}
   setBaseUrl(url: string) {
     this.baseUrl = url;
   }
