6b3da46632187bfe0eefc0722eebf95cca28ff19
guy arieli <guy.arieli@blinq.io>
2023-11-29 16:20:14 +0200

    fixes

--- tests/launch_page.test.js
+++ tests/launch_page.test.js
@@ -4,11 +4,13 @@ import { closeBrowser } from "../build/lib/init_browser.js";
 const name = "login";
 const path = "/";
 const elements = {
-  sign_in: [[{ role: ["link", { name: "{signin}" }] }]],
-  username: [[{ css: 'input[name="login"]' }]],
+  sign_in: [[{ role: ["link", { name: "{signin}" }] }], [{ css: "a[href='/login']" }]],
+  username: {
+    locators: [{ css: 'input[name="login"]' }],
+  },
   loginButton: [[{ role: ["button", { name: "Sign in" }] }]],
 };
-const context = await initContext(path, true, true);
+const context = await initContext(path, true, false);
 const login = async function () {
   let info = null;
   info = await context.stable.click(elements.sign_in, { signin: "Sign in" });
