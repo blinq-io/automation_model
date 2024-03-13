1073195148e33dfc1b1fecfd1a972516a3273aa4
guy arieli <guy.arieli@blinq.io>
2024-03-13 17:18:06 +0200

    update

--- src/drawRect.ts
+++ src/drawRect.ts
@@ -1,13 +1,7 @@
 import { createReadStream, createWriteStream } from "fs";
 import { decodePNGFromStream, encodePNGToStream } from "pureimage";
 
-const drawRectangle = async (
-  inputFilePath: string,
-  x: number,
-  y: number,
-  width: number,
-  height: number
-) => {
+const drawRectangle = async (inputFilePath: string, x: number, y: number, width: number, height: number) => {
   try {
     const img1 = await decodePNGFromStream(createReadStream(inputFilePath));
     const ctx = img1.getContext("2d");
@@ -18,7 +12,7 @@ const drawRectangle = async (
     ctx.fillRect(x + width + 1, y - 1, 2, height + 2);
 
     await encodePNGToStream(img1, createWriteStream(inputFilePath));
-    console.log("Rectangle drawn successfully", inputFilePath);
+    //console.log("Rectangle drawn successfully", inputFilePath);
   } catch (err) {
     console.error(err);
   }
