12e1364aeeea41b2081817909bb627da6a9ec50a
Aviral Gupta <aviral.gupta@blinq.io>
2024-04-18 13:30:35 +0530

    fix wrong  screenshots not appearing in the case  of multiple examples

--- src/auto_page.ts
+++ src/auto_page.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 import path from "path";
 import type { TestContext } from "./test_context.js";
 let context: TestContext | null = null;
+let reportFolder = "";
 const navigate = async (path = "") => {
   let url = null;
   if (path === null) {
@@ -27,7 +28,12 @@ const _findEmptyFolder = (folder?: string) => {
   }
   return path.join(folder, nextIndex.toString());
 };
-const initContext = async (path: string, doNavigate = true, headless = false, world: any = null) => {
+const initContext = async (
+  path: string,
+  doNavigate = true,
+  headless = false,
+  world: any = null
+) => {
   if (context) {
     return context;
   }
@@ -38,9 +44,11 @@ const initContext = async (path: string, doNavigate = true, headless = false, wo
   if (world) {
     world.context = context;
     world.screenshot = true;
-    const reportFolder = _findEmptyFolder();
-    if (world.attach) {
-      world.attach(reportFolder, { mediaType: "text/plain" });
+    if (!reportFolder) {
+      const reportFolder = _findEmptyFolder();
+      if (world.attach) {
+        world.attach(reportFolder, { mediaType: "text/plain" });
+      }
     }
     world.screenshotPath = reportFolder + "/screenshots/";
     if (!fs.existsSync(world.screenshotPath)) {
