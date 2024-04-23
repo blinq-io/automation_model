850c699cec5931b84163f839d4c354d013eda987
guy arieli <guy.arieli@blinq.io>
2024-04-23 15:52:27 +0300

    update test

--- tests/launch_page.test.js
+++ tests/launch_page.test.js
@@ -4,35 +4,32 @@ import { closeBrowser } from "../build/lib/init_browser.js";
 const name = "login";
 const path = "/";
 const elements = {
-  sign_in: {
-    locators: [{ role: ["link", { name: "{signin}" }] }, { css: "a[href='/login']" }],
+  loginButton: {
+    locators: [{ role: ["button", { name: "{signin}" }] }],
   },
   username: {
-    locators: [{ css: 'input[name="login"]' }],
+    locators: [{ css: 'input[name="username"]' }],
   },
   password: {
-    locators: [{ css: 'input[name="password"]' }],
-  },
-  loginButton: {
-    locators: [{ role: ["button", { name: "Sign in" }] }],
+    locators: [{ css: "#password" }],
   },
 };
 const context = await initContext(path, true, true);
 const login = async function () {
   let info = null;
-  info = await context.stable.verifyTextExistInPage("github", {});
-  info = await context.stable.click(elements.sign_in, { signin: "Sign in" });
+  info = await context.stable.verifyTextExistInPage("Accepted usernames are:", {});
+  info = await context.stable.click(elements.loginButton, { signin: "LOGIN" });
   console.log("info click sign in", JSON.stringify(info, null, 2));
   info = await context.stable.fill(elements.username, "guy");
   console.log("info fill username", JSON.stringify(info, null, 2));
   info = await context.stable.fill(elements.password, "guy");
   console.log("info fill password", JSON.stringify(info, null, 2));
-  info = await context.stable.click(elements.loginButton);
+  info = await context.stable.click(elements.loginButton, { signin: "LOGIN" });
   console.log("info click login", JSON.stringify(info, null, 2));
   info = await context.stable.verifyElementExistInPage({
     locators: [
       {
-        text: "Incorrect username or password.",
+        text: "Invalid username or password",
       },
     ],
   });
