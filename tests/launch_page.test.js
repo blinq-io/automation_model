e4268ea1a581532c0b7bb5b031c54ce5cc396b1d
guy arieli <guy.arieli@blinq.io>
2024-04-23 16:44:42 +0300

    update

--- tests/launch_page.test.js
+++ tests/launch_page.test.js
@@ -5,7 +5,7 @@ const name = "login";
 const path = "/";
 const elements = {
   loginButton: {
-    locators: [{ role: ["button", { name: "LOGIN" }] }],
+    locators: [{ role: ["button", { name: "{signin}" }] }],
   },
   username: {
     locators: [{ css: 'input[name="username"]' }],
@@ -18,13 +18,13 @@ const context = await initContext(path, true, true);
 const login = async function () {
   let info = null;
   info = await context.stable.verifyTextExistInPage("Accepted usernames are:", {});
-  info = await context.stable.click(elements.loginButton);
+  info = await context.stable.click(elements.loginButton, { signin: "Login" });
   console.log("info click sign in", JSON.stringify(info, null, 2));
   info = await context.stable.fill(elements.username, "guy");
   console.log("info fill username", JSON.stringify(info, null, 2));
   info = await context.stable.fill(elements.password, "guy");
   console.log("info fill password", JSON.stringify(info, null, 2));
-  info = await context.stable.click(elements.loginButton);
+  info = await context.stable.click(elements.loginButton, { signin: "Login" });
   console.log("info click login", JSON.stringify(info, null, 2));
   info = await context.stable.verifyElementExistInPage({
     locators: [
@@ -38,12 +38,9 @@ const login = async function () {
 try {
   await login();
 } catch (e) {
-  if (e.info) {
-    console.log("info", JSON.stringify(e.info, null, 2));
-  }
   try {
     const html = await context.stable.page.content();
-    console.log("html content", html);
+    console.log("html", html);
   } catch (e) {
     console.log(e);
     console.log("unable to get html content");
