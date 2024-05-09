39a2ae0c5545ca6bbcce028eaa47b086d20bced4
guy arieli <guy.arieli@blinq.io>
2024-05-09 14:24:42 +0300

    update

---
+++ src/api.ts
@@ -0,0 +1,83 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import tunnel, { ProxyOptions } from "tunnel";
+class Api {
+  private axiosClient: AxiosInstance;
+  constructor(public logger: any) {
+    if (!logger) {
+      this.logger = console;
+    }
+    const agent = this.getProxyObject(process.env.PROXY);
+    this.logger.debug("proxy object");
+    this.logger.debug(agent);
+    this.axiosClient = axios.create({
+      httpsAgent: agent,
+      proxy: false,
+    });
+  }
+  getProxyObject(proxy?: string) {
+    if (!proxy) {
+      proxy = process.env.PROXY;
+    }
+    if (!proxy) {
+      return null;
+    }
+
+    try {
+      const url = new URL(proxy);
+      const proxyObject: ProxyOptions = {
+        host: url.hostname,
+        port: Number(url.port),
+      };
+
+      const { username, password } = url;
+
+      if (username && password) {
+        proxyObject.proxyAuth = `${username}:${password}`;
+      }
+      return tunnel.httpsOverHttp({ proxy: proxyObject });
+    } catch (error) {
+      this.logger.error("Error while parsing proxy url");
+      this.logger.error(error);
+      return null;
+    }
+  }
+
+  public async axiosClientRequest<T = any>(config: AxiosRequestConfig): Promise<AxiosResponse<T, any>> {
+    this.logger.debug("axios request");
+    this.logger.debug(config);
+    for (let i = 0; i < 2; i++) {
+      try {
+        const res = this.axiosClient<T>(config);
+        this.logger.info("axios response");
+        this.logger.info(res);
+        return res;
+      } catch (error: any) {
+        this.logger.info("axios error");
+        this.logger.info(error);
+        if (i === 1) {
+          this.logger.error("Error while sending request " + error.message);
+          throw error;
+        }
+
+        if (
+          (error.response && error.response.data.includes("self signed certificate")) ||
+          (error.message && error.message.includes("self signed certificate"))
+        ) {
+          process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+          this.logger.info("NODE_TLS_REJECT_UNAUTHORIZED = " + process.env.NODE_TLS_REJECT_UNAUTHORIZED);
+        }
+      }
+    }
+    throw new Error("Request failed after 2 attempts"); // Throw an error or return a value explicitly here
+  }
+  public setProxy(proxy?: string) {
+    const agent = this.getProxyObject(proxy);
+    this.logger.info(agent);
+    this.axiosClient = axios.create({ proxy: false, httpsAgent: agent });
+  }
+  public async request<T = any>(config: AxiosRequestConfig): Promise<AxiosResponse<T, any>> {
+    return await this.axiosClientRequest<T>(config);
+  }
+}
+
+export { Api };
--- src/test_context.ts
+++ src/test_context.ts
@@ -1,16 +1,16 @@
 import { BrowserContext, Page, Browser as PlaywrightBrowser } from "playwright";
 import { Environment } from "./environment.js";
 import { StableBrowser } from "./stable_browser.js";
+import { Api } from "./api.js";
 
 class TestContext {
-  stable: StableBrowser|null = null
-  browser: PlaywrightBrowser|null = null
-  playContext: BrowserContext | null = null
-  page: Page | null = null
-  environment: Environment|null = null
-  reportFolder: string|null = null
-  constructor(
-  ) {
-  }
+  stable: StableBrowser | null = null;
+  browser: PlaywrightBrowser | null = null;
+  playContext: BrowserContext | null = null;
+  page: Page | null = null;
+  environment: Environment | null = null;
+  reportFolder: string | null = null;
+  api: Api | null = null;
+  constructor() {}
 }
 export { TestContext };
