3369eea7d04cce29690af6792bd8bcf7a40b27a3
guy arieli <guy.arieli@blinq.io>
2024-07-31 13:23:17 +0300

    update

--- src/utils.ts
+++ src/utils.ts
@@ -21,11 +21,11 @@ async function decrypt(encryptedText: string, key: string | null = null, totpWai
   if (encryptedText.startsWith("totp:")) {
     encryptedText = encryptedText.substring(5);
     const bytes = CryptoJS.AES.decrypt(encryptedText, key);
-    encryptedText =  bytes.toString(CryptoJS.enc.Utf8);
+    encryptedText = bytes.toString(CryptoJS.enc.Utf8);
     let { otp, expires } = TOTP.generate(encryptedText);
-    if(totpWait) {
-      // expires is in unix time, check if we have at least 25 seconds left, if it's less than wait for the expires time
-      if (expires - Date.now() < 25000) {
+    if (totpWait) {
+      // expires is in unix time, check if we have at least 10 seconds left, if it's less than wait for the expires time
+      if (expires - Date.now() < 10000) {
         await new Promise((resolve) => setTimeout(resolve, (expires - Date.now() + 1000) % 30000));
         ({ otp, expires } = TOTP.generate(encryptedText));
       }
@@ -39,9 +39,9 @@ function _findKey() {
   if (process.env.PROJECT_ID) {
     return process.env.PROJECT_ID;
   }
-  if(process.env.PROJECT_PATH) {
+  if (process.env.PROJECT_PATH) {
     return path.basename(process.env.PROJECT_PATH);
-}
+  }
   // find the project folder name
   let folder = process.cwd();
   // extract the base folder name
