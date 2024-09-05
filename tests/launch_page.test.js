23cb047063279a1b91289632e93f31b726492c7b
guy arieli <guy.arieli@blinq.io>
2024-09-05 17:42:15 +0300

    update

--- tests/launch_page.test.js
+++ tests/launch_page.test.js
@@ -14,13 +14,15 @@ const elements = {
     locators: [{ css: "#password" }],
   },
 };
-const context = await initContext(path, true, true);
+const context = await initContext(path, false, true);
 const login = async function () {
   let info = null;
   info = await context.stable.verifyTextExistInPage("Accepted usernames are:", {});
-  console.log(info.log)
+  console.log(info.log);
   info = await context.stable.click(elements.loginButton, { signin: "Login" });
-  console.log(info.log)
+  await switchApp("shop");
+
+  console.log(info.log);
   //console.log("info click sign in", JSON.stringify(info, null, 2));
   info = await context.stable.fill(elements.username, "guy");
   console.log(info.log);
