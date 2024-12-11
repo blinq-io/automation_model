26d649e698bc0adb81506f2195b0fa0e17b82209
madhav-blinq <147692216+madhav-blinq@users.noreply.github.com>
2024-12-11 14:56:53 +0530

    fix: set global flag when closing popups

--- src/stable_browser.ts
+++ src/stable_browser.ts
@@ -679,9 +679,19 @@ class StableBrowser {
         }
         if (result.foundElements.length > 0) {
           let dialogCloseLocator = result.foundElements[0].locator;
-          await dialogCloseLocator.click();
-          // wait for the dialog to close
-          await dialogCloseLocator.waitFor({ state: "hidden" });
+
+          try {
+            await scope?.evaluate(() => {
+              window.__isClosingPopups = true;
+            });
+            await dialogCloseLocator.click();
+            // wait for the dialog to close
+            await dialogCloseLocator.waitFor({ state: "hidden" });
+          } catch (error) {
+            await scope?.evaluate(() => {
+              window.__isClosingPopups = false;
+            });
+          }
           return { rerun: true };
         }
       }
