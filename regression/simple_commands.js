c7d9f3728e8c7937425c5c8dd9b61f37ecd02640
guy arieli <guy.arieli@blinq.io>
2024-12-15 11:22:56 +0200

    add test

---
+++ regression/simple_commands.js
@@ -0,0 +1,50 @@
+import { initContext, closeContext } from "../build/lib/auto_page.js";
+import fs from "fs";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+
+export const handlers = [
+  // Intercept "GET https://example.com/user" requests...
+  http.post("*/api/runs/locate-element/locate", () => {
+    // ...and respond to them using this JSON response.
+    return HttpResponse.json({
+      status: true,
+      result: {
+        elementNumber: 2,
+        reason:
+          "The element with elementNumber 2 is a button with the name 'Login', which matches the task requirement to click on 'Login button'.",
+        name: "locate_element",
+      },
+    });
+  }),
+];
+export const server = setupServer(...handlers);
+let context = null;
+
+//{"status":true,"result":{"elementNumber":2,"reason":"The element with elementNumber 2 is a button with the name 'Login', which matches the task requirement to click on 'Login button'.","name":"locate_element"}}
+describe("Actions Tests", function () {
+  before(async function () {
+    server.listen();
+    // check if temp directory exists
+    if (!fs.existsSync("temp")) {
+      fs.mkdirSync("temp");
+    }
+    console.log("Actions Tests: before");
+  });
+  beforeEach(async function () {
+    context = await initContext("/", true, false);
+  });
+  afterEach(async function () {
+    await closeContext();
+  });
+  after(async function () {
+    server.close();
+    console.log("Actions Tests: after");
+  });
+
+  it("simple click", async function () {
+    let info = {};
+    info.log = "";
+    const element = await context.stable.simpleClick("Login button");
+  });
+});
