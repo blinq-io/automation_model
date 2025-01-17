bb0ce988c0744b1dbaf21c41d2c2f962e1fc4e71
guy arieli <guy.arieli@blinq.io>
2025-01-17 10:22:06 +0200

    fix a bug

--- src/stable_browser.ts
+++ src/stable_browser.ts
@@ -391,6 +391,9 @@ class StableBrowser {
     const query = _convertToRegexQuery(text1, regex1, !partial1, ignoreCase);
     const locator = scope.locator(query);
     const count = await locator.count();
+    if (!tag1) {
+      tag1 = "*";
+    }
     const randomToken = Math.random().toString(36).substring(7);
     let tagCount = 0;
     for (let i = 0; i < count; i++) {
