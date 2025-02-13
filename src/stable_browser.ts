837b8e3be0e90cf716f4c7b6bfde1a5734ec64cd
adibalak <aditya@blinq.io>
2025-02-13 16:07:52 +0530

    Chore: commented out console errors

--- src/stable_browser.ts
+++ src/stable_browser.ts
@@ -1465,7 +1465,7 @@ class StableBrowser {
         this._highlightElements(element)
           .then(async () => {
             await new Promise((resolve) => setTimeout(resolve, 1000));
-            this._unhighlightElements(element).then(() =>
+            this._unhighlightElements(element).then(() => {}
               // console.log(`Unhighlighting vrtr in recorder is successful`)
             );
           })
@@ -2142,7 +2142,7 @@ class StableBrowser {
           })
           .catch((e) => {
             // ignore
-            console.error(`Could not highlight node : ${e}`);
+            // console.error(`Could not highlight node : ${e}`);
           });
       } else {
         scope
@@ -2180,7 +2180,7 @@ class StableBrowser {
           })
           .catch((e) => {
             // ignore
-            console.error(`Could not highlight css: ${e}`);
+            // console.error(`Could not highlight css: ${e}`);
           });
       }
     } catch (error) {
@@ -2201,7 +2201,7 @@ class StableBrowser {
                 node.style.outline = "";
               } else {
                 node.style.outline = node.__previousOutline;
-              }
+              } 
             }
           })
           .then(() => {
@@ -2231,11 +2231,11 @@ class StableBrowser {
           .then(() => {
           })
           .catch((e) => {
-            console.error(`Error while unhighlighting element in css: ${e}`);
+            // console.error(`Error while unhighlighting element in css: ${e}`);
           });
       }
     } catch (error) {
-      console.debug(error);
+      // console.debug(error);
     }
   }
 
@@ -2404,7 +2404,9 @@ class StableBrowser {
                 .then(async () => {
                   // console.log(`Unhighlighted frame dataAttribute successfully`);
                 })
-                .catch((e) => console.error(e));
+                .catch((e) => {}
+                  //  console.error(e)
+              );
             });
           }
           const element = await frame.locator(dataAttribute).first();
@@ -2559,7 +2561,7 @@ class StableBrowser {
                 this._highlightElements(frame, dataAttribute)
                   .then(async () => {
                     await new Promise((resolve) => setTimeout(resolve, 1000));
-                    this._unhighlightElements(frame, dataAttribute).then(() =>
+                    this._unhighlightElements(frame, dataAttribute).then(() => {}
                       // console.log(`Unhighlighting vrtr in recorder is successful`)
                     );
                   })
