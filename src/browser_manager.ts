c41dd8d7803b9bb998335e5fab652a2617895cd6
Khaled-Blinqio <khaled@blinq.io>
2025-07-08 16:00:52 +0300

    Dont put use-gtk in global args

--- src/browser_manager.ts
+++ src/browser_manager.ts
@@ -136,7 +136,7 @@ class Browser {
     } else if (!aiConfig.noViewport) {
       viewport = { width: 1280, height: 800 };
     }
-    const args = ["--ignore-https-errors", "--ignore-certificate-errors", "--use-gtk"];
+    const args = ["--ignore-https-errors", "--ignore-certificate-errors"];
     if (process.env.CDP_LISTEN_PORT) {
       args.push(`--remote-debugging-port=${process.env.CDP_LISTEN_PORT}`);
     }
