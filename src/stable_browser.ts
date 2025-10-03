28f0abe5c15ae991718ebcc01170c943622488fc
Yogesh Singh <yogesh.singh@blinq.io>
2025-10-03 12:07:46 +0530

    AIC-4979: fixed the testData for verifyProperty

--- src/stable_browser.ts
+++ src/stable_browser.ts
@@ -2776,7 +2776,7 @@ class StableBrowser {
     let expectedValue;
     try {
       await _preCommand(state, this);
-      expectedValue = await this._replaceWithLocalData(value, world);
+      expectedValue = await replaceWithLocalTestData(state.value, world, true, true, this.context);
       state.info.expectedValue = expectedValue;
       switch (property) {
         case "innerText":
