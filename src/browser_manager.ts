df8a75c67e8f01beea7f1a1f3403983fca0a8e6a
Aditya Balakrishnan <aditya@blinq.io>
2025-11-10 18:21:03 +0530

    remove commented-out code for remote debugging address in Browser class

--- src/browser_manager.ts
+++ src/browser_manager.ts
@@ -129,7 +129,6 @@ class Browser {
 
     if (process.env.CDP_LISTEN_PORT) {
       args.push(`--remote-debugging-port=${process.env.CDP_LISTEN_PORT}`);
-      // args.push(`--remote-debugging-address=0.0.0.0`);
     }
 
     if (process.env.REMOTE_ORIGINS_URL) {
