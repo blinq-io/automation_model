20df761dcb40bdeeafe00ce8573dd4a6690e27ce
madhav-blinq <147692216+madhav-blinq@users.noreply.github.com>
2025-11-17 11:04:22 +0530

    fix tests: add not undefined check

--- src/stable_browser.ts
+++ src/stable_browser.ts
@@ -4591,7 +4591,9 @@ class StableBrowser {
     });
   }
   async beforeStep(world, step) {
-    await this.context.playContext.tracing.group(`Step: ${step.pickleStep.text}`);
+    if (step?.pickleStep) {
+      await this.context.playContext.tracing.group(`Step: ${step.pickleStep.text}`);
+    }
     this.stepTags = [];
     if (!this.beforeScenarioCalled) {
       this.beforeScenario(world, step);
