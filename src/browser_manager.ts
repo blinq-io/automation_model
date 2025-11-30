879cd1afab2d1226a5a1f27f758ca334d061a520
adibalak <adibalak@users.noreply.github.com>
2025-11-30 12:01:59 +0000

    Prettified Code!

--- src/browser_manager.ts
+++ src/browser_manager.ts
@@ -150,10 +150,10 @@ class Browser {
       args.push(`--remote-allow-origins=${process.env.REMOTE_ORIGINS_URL}`);
     }
 
-    if(process.env.MAXIMIZE_BROWSER==="true"){
+    if (process.env.MAXIMIZE_BROWSER === "true") {
       console.log("Starting browser maximized for remote recorder");
       args.push(`--start-maximized`);
-    } 
+    }
 
     let useSessionFolder = false;
     if (!extensionPath && userDataDirPath) {
