474b449e28f6e4ea3cfd9d957ef6848715bfdcec
Aditya Balakrishnan <aditya@blinq.io>
2025-11-30 17:31:06 +0530

    fix: background docker container.

--- src/browser_manager.ts
+++ src/browser_manager.ts
@@ -161,9 +161,10 @@ class Browser {
       args.push(`--remote-allow-origins=${process.env.REMOTE_ORIGINS_URL}`);
     }
 
-    if (process.env.REMOTE_RECORDER === "true") {
+    if(process.env.MAXIMIZE_BROWSER==="true"){
+      console.log("Starting browser maximized for remote recorder");
       args.push(`--start-maximized`);
-    }
+    } 
 
     let useSessionFolder = false;
     await ensurePlaywright();
